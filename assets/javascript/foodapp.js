6ee8740a3caff94587f83f5fdc5b3f979cbc602d
Aisha <aishaprograms@gmail.com>
2016-11-19 14:30:39 -0600

    Food app has minimal functionality

--- assets/javascript/foodapp.js
+++ assets/javascript/foodapp.js
@@ -1,23 +1,17 @@
-var foods = {
-    nuts: ['cashews', 'almonds', 'hazelnuts',
-        'brazil nuts', 'peanut butter', 'pumpkin seeds',
-        'walnuts'
-    ],
-    meats: ['lean pork', 'lean beef', ' chicken',
-        'turkey', 'salmon', 'eggs', 'tuna'
-    ],
-    vegetables: ['leafy greens', 'spinach', 'romaine',
-        'sweet potato', 'edamame', 'asparagus'
-    ],
-    grains: ['oatmeal', 'popcorn',
-        'brown rice', 'beans', 'legumes', 'whole grain cereal',
-        'quinoa'
-    ],
-    fruits: ['bananas', 'apples', 'oranges', 'blueberries',
-        'melons'
-    ],
-    quickFixes: ['caffeine', 'sugar', 'coffee', 'tea',
-        'chocolate', 'honey'
-    ],
-    dairy: ['yogurt', 'cheese']
+var database = firebase.database();
+
+document.querySelector('#search-food').onclick = function() {
+    var food = $('#food-input').val().trim();
+    var foodRef = database.ref('/home/foods/nuts/cashews');
+    foodRef.once('value').then(function(snapshot) {
+        var info = snapshot.val().info;
+        var foodName = snapshot.val().name;
+        var newDiv = $('<div>');
+        var newImg = $('<img>');
+        newImg.attr('src', 'assets/images/cashews.jpg');
+        newDiv.append(foodName);
+        newDiv.append(newImg);
+        newDiv.append(info);
+        $('#test-div').append(newDiv);
+    });
 };
