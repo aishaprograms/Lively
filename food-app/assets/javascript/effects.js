ba0b50bc3e856e752dff9a1465da714e327836d3
Aisha <aishaprograms@gmail.com>
2016-11-21 16:47:50 -0600

    Created feature directories

---
+++ food-app/assets/javascript/effects.js
@@ -0,0 +1,15 @@
+ //scroll animation to section on page
+ $(function() {
+     $('a[href*="#"]:not([href="#"])').click(function() {
+         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+             var target = $(this.hash);
+             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+             if (target.length) {
+                 $('html, body').animate({
+                     scrollTop: target.offset().top
+                 }, 1000);
+                 return false;
+             }
+         }
+     });
+ });
