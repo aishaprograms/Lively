bff57d0cbff05988f51cdd5ac0545af5633a4407
Josh Kim <kangsan@Joshs-MacBook-Air.local>
2016-11-22 17:46:42 -0600

    The checkboxes have userInput as class on html, copy and paste the removeA function, chosenFoods array variable, and the .userInput on click.

--- recipe-app/assets/javascript/functionality.js
+++ recipe-app/assets/javascript/functionality.js
@@ -20,46 +20,36 @@ $(document).ready(function() {
     //an array in the javascript
     var chosenFoods = [];
 
-    //Terence made the landing/home page with the carousel component. The carousel component is going to be used as the 
-    //questionaire and this function populates a certain <div> with the "foods" array as buttons(answer choices) 
+function removeA(arr){
+var what, a= arguments, L= a.length, ax;
+while(L> 1 && arr.length){
+    what= a[--L];
+    while((ax= arr.indexOf(what))!= -1){
+        arr.splice(ax, 1);
+    }
+}
+return arr;
+}
 
-    //*** note that the carousel componenet was a change on the front end that was made at the end of class, and so
-    //im not sure if this function will work in the same manner as it would on a regular <div>
+    $('.userInput').on('click', function() {
+        
+        var name = this.id;
 
-    // function foodChoiceButtonmaker() {
-
-    //     for (var i = 0; i < foods.length; i++) {
-
-    //         var b = $('<button>');
-    //         b.attr('data-foodName', foods[i]);
-    //         b.addClass('foodChoice');
-    //         b.text(foods[i]);
-
-    //         $('#eat').append(b); //replace "#eat" with whichever div id is the display for the carousel.  
-    //     }
-
-    // };
-
-
-    // this event handler/listener activates the above button making function when the user presses the 
-    //"lets get started" button which should be on the first carousel page. So essentially the idea is that
-    //the first question in the questionaire is asked after you press "lets get started" button.  But since
-    //now were working with a carousel, the questionaire simply starts by sliding carousel pages.  This code
-    //was oringally written for an entire page change dedicated to the questionaire,instead of changin pages
-    //on a carousel so you may have to make minor changes as deemed necessary
-    // $('start-button').on('click', foodChoiceButtonmaker());
+        if(chosenFoods.includes(name)){
+            for (var i = 0; i <chosenFoods.length; i++) {
+                if(this.id==chosenFoods[i]){
+                    removeA(chosenFoods,this.id)
 
 
-    //this on click stores the user's favorite foods into the "chosenFoods" array.
-    //(again this should be done though firebase if possible).
-    // $('.foodChoice').on('click', function() {
-    //     var name = $(this).attr('data-foodName');
-    //     chosenFoods.push(name);
-    //     $(this).remove();
+                }
+            }
+        }
 
-    //     console.log(chosenFoods);
+        else{
+        chosenFoods.push(name);
+        }
 
-    // });
+    });
 
     //this on click compiles the answers (as of now only the first question answers) and spits back your 
     //result(recipes) into the appropriate place on the second html which should be found on hadi's branch.  
@@ -78,7 +68,7 @@ $(document).ready(function() {
         function recipeGrabber() {
 
             //these were used for the original html design, to clear the page before appending the results.
-            $("#recipe-section").empty();
+            // $("#recipe-section").empty();
 
             //calling api
             $.ajax({
@@ -91,7 +81,7 @@ $(document).ready(function() {
 
                 //these three blocks of code are ugly but i did them just to see if it was working.  if pulls the image data and recipe
                 //titles from the first three recipe objects that are pulled from the api and displays them on the page.
-                var div1 = $('<div>');
+                // var div1 = $('<div>');
                 var link1 = $('<a>');
                 link1.attr('href', result.hits[0].recipe.url);
                 link1.attr('target', '_blank');
@@ -103,44 +93,44 @@ $(document).ready(function() {
                 var title1 = $('<h6>');
                 title1.text(recipe1);
                 title1.addClass('truncate');
-                link1.append(title1);
+                // link1.append(title1);
                 link1.append(image1);
-                div1.append(link1);
+                $('#card-image1').append(link1);
                 $('#recipe-section').append(div1); //change "#recipe-section" to whichever div necessary
 
-                var div2 = $('<div>');
-                var link2 = $('<a>');
-                link2.attr('href', result.hits[1].recipe.url);
-                link2.attr('target', '_blank');
-                var imageURL2 = result.hits[1].recipe.image;
-                var image2 = $('<img>');
-                image2.addClass('responsive-img');
-                image2.attr('src', imageURL2);
-                var recipe2 = result.hits[1].recipe.label;
-                var title2 = $('<h6>');
-                title2.addClass('truncate');
-                title2.text(recipe2);
-                link2.append(title2);
-                link2.append(image2);
-                div2.append(link2);
-                $('#recipe-section').append(div2); //change "#recipe-section" to whichever div necessary
+                // var div2 = $('<div>');
+                // var link2 = $('<a>');
+                // link2.attr('href', result.hits[1].recipe.url);
+                // link2.attr('target', '_blank');
+                // var imageURL2 = result.hits[1].recipe.image;
+                // var image2 = $('<img>');
+                // image2.addClass('responsive-img');
+                // image2.attr('src', imageURL2);
+                // var recipe2 = result.hits[1].recipe.label;
+                // var title2 = $('<h6>');
+                // title2.addClass('truncate');
+                // title2.text(recipe2);
+                // link2.append(title2);
+                // link2.append(image2);
+                // div2.append(link2);
+                // $('#recipe-section').append(div2); //change "#recipe-section" to whichever div necessary
 
-                var div3 = $('<div>');
-                var link3 = $('<a>');
-                link3.attr('href', result.hits[2].recipe.url);
-                link3.attr('target', '_blank');
-                var imageURL3 = result.hits[2].recipe.image;
-                var image3 = $('<img>');
-                image3.addClass('responsive-img');
-                image3.attr('src', imageURL3);
-                var recipe3 = result.hits[2].recipe.label;
-                var title3 = $('<h6>');
-                title3.text(recipe3);
-                title3.addClass('truncate');
-                link3.append(title3);
-                link3.append(image3);
-                div3.append(link3);
-                $('#recipe-section').append(div3); //change "#recipe-section" to whichever div necessary
+                // var div3 = $('<div>');
+                // var link3 = $('<a>');
+                // link3.attr('href', result.hits[2].recipe.url);
+                // link3.attr('target', '_blank');
+                // var imageURL3 = result.hits[2].recipe.image;
+                // var image3 = $('<img>');
+                // image3.addClass('responsive-img');
+                // image3.attr('src', imageURL3);
+                // var recipe3 = result.hits[2].recipe.label;
+                // var title3 = $('<h6>');
+                // title3.text(recipe3);
+                // title3.addClass('truncate');
+                // link3.append(title3);
+                // link3.append(image3);
+                // div3.append(link3);
+                // $('#recipe-section').append(div3); //change "#recipe-section" to whichever div necessary
 
 
             });
@@ -148,18 +138,22 @@ $(document).ready(function() {
         //************
         //temporary code
         //************
-        chosenFoods = ['cashews', 'almonds', 'hazelnuts'];
+        
         //************
         //************
         //this simply randomizes recipeTopics for the user each time he logs on to use it, so it doesnt pop out the same
         //recipes all the time.
         recipeTopic = chosenFoods[Math.floor(Math.random() * chosenFoods.length)];
+    
 
         //this then takes the randomly chosen recipeTopic from chosenFoods array and then calls the api function
         console.log(recipeTopic);
-        queryURL = "https://crossorigin.me/https://api.edamam.com/search?q=" + recipeTopic + "&app_id=a131e0d4&app_key=b0fab7e7fe1dbcd87138b0464ab43cb5&from=0&to=3";
-        recipeGrabber(); //api function being evoked after query is updated 
 
+        for (var i = 0; i < 3; i++) {
+        recipeTopic = chosenFoods[Math.floor(Math.random() * chosenFoods.length)];    
+        queryURL = "https://crossorigin.me/https://api.edamam.com/search?q=" + recipeTopic + "&app_id=a131e0d4&app_key=b0fab7e7fe1dbcd87138b0464ab43cb5&from=0&to=3";
+        recipeGrabber(); //api function being evoked after query is updated
+        }
 
     });
 
