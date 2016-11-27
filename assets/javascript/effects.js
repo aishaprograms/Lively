bd7c6caa94fe950c34c5f9ffc3e0ad75cae4b61e
Mohammed Hadiyousufi <reach.hadiy@gmail.com>
2016-11-27 00:45:17 -0600

    Changed materizlize checkboxe's border and check to white color

--- assets/javascript/effects.js
+++ assets/javascript/effects.js
@@ -1,41 +1,46 @@
- //scroll animation to section on page
- $(function() {
-     $('a[href*="#"]:not([href="#"])').click(function() {
-         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-             var target = $(this.hash);
-             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-             if (target.length) {
-                 $('html, body').animate({
-                     scrollTop: target.offset().top
-                 }, 1000);
-                 return false;
-             }
-         }
-     });
- });
+ //Pressing Enter Key advances NEXT section on carousel
 
- $('.carousel.carousel-slider').carousel({
-     full_width: true
- });
- $('#start-button').on('click', function() {
-     $('.carousel').carousel('next');
- });
- $('.modal').modal();
+ $(document).ready(function() {
 
- $('.parallax').parallax();
 
- // Slick Carousel code for slider
- $('#index-carousel').slick({
-     dots: false,
-     arrows: false
- });
- $('#start-button').on('click', function() {
-     $('#index-carousel').slick('slickNext');
- });
- //done-btn changed to food-button
- $("#food-button").click(function() {
-     $('.carousel').carousel('next');
-     $('#index-carousel').slick('slickNext');
- });
+             //scroll animation to suggestions section on page
+             $(function() {
+                 $('a[href*="#"]:not([href="#"])').click(function() {
+                     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+                         var target = $(this.hash);
+                         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                         if (target.length) {
+                             $('html, body').animate({
+                                 scrollTop: target.offset().top
+                             }, 1000);
+                             return false;
+                         }
+                     }
+                 });
+             });
 
- $('label').addClass('white-text');
+             $('.carousel.carousel-slider').carousel({
+                 full_width: true
+             });
+             $('#start-button').on('click', function() {
+                 $('.carousel').carousel('next');
+             });
+             $('.modal').modal();
+
+             $('.parallax').parallax();
+
+             // Slick Carousel code for slider
+             $('#index-carousel').slick({
+                 dots: false,
+                 arrows: false
+             });
+             $('#start-button').on('click', function() {
+                 $('#index-carousel').slick('slickNext');
+             });
+             //done-btn changed to food-button
+             $("#food-button").click(function() {
+                 $('.carousel').carousel('next');
+                 $('#index-carousel').slick('slickNext');
+             });
+
+             $('label').addClass('white-text');
